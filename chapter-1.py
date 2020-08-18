0ef790dfc399351f7571379b4564ab6c35fa78a1
黄琛垚 <hcy_96@outlook.com>
2020-08-18 18:20:35 +0800

    save

--- chapter-1.py
+++ chapter-1.py
@@ -8,9 +8,15 @@ class ArticleVote:
         self._VOTE_SCORE = 432
         self._pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
 
+    def clear(self):
+        self._get_conn().flushall()
+
+    def _get_conn(self):
+        return redis.Redis(connection_pool=self._pool)
+
     # 对文章进行投票
     def article_vote(self, user: str, article: str):
-        r = redis.Redis(connection_pool=self._pool)
+        r = self._get_conn()
         cutoff = time.time() - self._ONE_WEEK_IN_SECOND
         # 检查是否还可以对文章进行投票
         if r.zscore('time:', article) < cutoff:
@@ -25,7 +31,7 @@ class ArticleVote:
 
     # 发布并获取文章
     def post_article(self, user: str, title: str, link: str):
-        r = redis.Redis(connection_pool=self._pool)
+        r = self._get_conn()
         article_id = str(r.incr('article:'))
 
         # 将发布文章的用户添加到文章的已投票用户名单里，
@@ -53,7 +59,7 @@ class ArticleVote:
 
     # 获取文章
     def get_articles(self, page_num: int, page_size=25, order='score:'):
-        r = redis.Redis(connection_pool=self._pool)
+        r = self._get_conn()
         start = (page_num - 1) * page_size
         end = start + page_size - 1
 
@@ -66,8 +72,36 @@ class ArticleVote:
 
         return articles
 
+    # 对文章进行分组
+    def add_remove_groups(self, article_id: str, to_add=[], to_remove=[]):
+        r = self._get_conn()
+        article = 'article:' + article_id
+        for group in to_add:
+            r.sadd('group:' + group, article)
+        for group in to_remove:
+            r.srem('group:' + group, article)
+
+    def get_group_articles(self, group: str, page_num: int, page_size=25, order='score:'):
+        r = self._get_conn()
+        key = order + group
+        if not r.exists(key):
+            r.zinterstore(key, ['group:' + group, order], aggregate='max')
+            r.expire(key, 60)
+        return self.get_articles(page_num, page_size, key)
+
 
 if __name__ == '__main__':
     a_v = ArticleVote()
-    a_v.post_article('000001', 'title000', 'link1')
+    a_v.clear()
+
+    user1, user2, user3 = 'user1', 'user2', 'user3'
+    a_v.post_article(user1, 'python', 'link')
+    a_v.post_article(user2, 'C#', 'link')
+    a_v.post_article(user3, 'eat', 'link')
+    a_v.post_article(user3, 'van', 'link')
+
+    a_v.add_remove_groups('article:1', ['programming'])
+    a_v.add_remove_groups('article:2', ['programming'])
+
     print(a_v.get_articles(1))
+    print(a_v.get_group_articles('programming', 1))
